f2a87e8105cb91c9c26a89d7cdb55af8e1eaa68f
Kaushalya Popat <73777703+kaushal9900@users.noreply.github.com>
2021-07-14 16:54:11 +0530

    Add files via upload

---
+++ Union.java
@@ -0,0 +1,28 @@
+class Union {
+	int parent[];
+	int rank[];	
+	public static int findParent(int node) {
+		if(node==parent[node]) return node;
+		return parent[node] = findParent(parent[node]);
+	}
+	public static void UnionByRank(int u,int v) {
+		u = findParent(u);
+		v = findParent(v);
+		if(rank[u]<rank[v]) {
+			parent[u] = v;
+		}else if(rank[u]>rank[v]) {
+			parent[v] = u;
+		}else {
+			parent[v] = u;
+			rank[u]++;
+		}
+	}
+	public static void init(int n) {
+		parent = new int[n+1];
+		rank = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			parent[i] = i;
+			rank[i] = 0;
+		}
+	}
+}
