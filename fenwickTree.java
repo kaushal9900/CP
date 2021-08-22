51a905659b336e81439895c0f81f453c7f9fcecd
Kaushalya Popat <73777703+kaushal9900@users.noreply.github.com>
2021-08-22 11:31:24 +0530

    Update fenwickTree.java

--- fenwickTree.java
+++ fenwickTree.java
@@ -2,19 +2,16 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 class fenwickTree {
-	int[] fenwick = new int[100005];
-	int[] arr = new int[100005]; // in this we will take intput
-	public static void update(int ind,int val) {
-		a[ind] +=val;
+	public void update(int ind,int val,int[] fen,int n) {		
 		while(ind<=n) {
 			fen[ind]+=val;
 			ind = ind + (ind&-ind);
 		}
 	}
-	public static int sum(int ind) {
+	public  int sum(int ind,int[] fen) {
 		int s = 0;
 		while(ind > 0) {
-			sum+=fen[ind];
+			s+=fen[ind];
 			ind = ind - (ind&-ind);
 		}
 		return s;
